0b0f42c554f9abb03222dd476e2d56061eea9f9f
Justin Waugh <jwaugh@frogsquared.com>
2018-04-26 10:32:37 -0400

    using koa

--- src/backend.js
+++ src/backend.js
@@ -1,20 +1,24 @@
-import express from 'express';
-import Register from 'common/api/register';
+import Koa from 'koa';
+import logger from 'koa-logger';
+import Router from 'koa-router';
+import koaBody from 'koa-body';
+import serve from 'koa-static';
+import Api from 'common/api/api';
 
-const app = express();
-app.use(express.static(__dirname));
+const env = process.env.NODE_ENV || 'development';
 
-app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: __dirname});
-});
+const app = module.exports = new Koa();
+app.use(logger());
+app.use(koaBody());
+app.use(serve(__dirname));
 
-app.get('/api/register', (req, res) => {
-    Register.registerUser();
-    res.json('{}');
-});
+const router = new Router();
+router.get('/api/login', Api.login);
+router.get('/api/logout', Api.logout);
+router.get('/api/register', Api.registerUser);
+router.get('/api/games', Api.listGames);
+router.get('/api/game/create', Api.createGame);
+router.get('/api/game/state', Api.getGameState);
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
-});
+app.use(router.routes());
+app.listen('3000');
---
+++ src/common/api/api.js
@@ -0,0 +1,35 @@
+class Api {
+    static login(ctx) {
+
+    }
+
+    static logout(ctx) {
+
+    }
+
+    static registerUser(ctx) {
+
+    }
+
+    static handlePasswordResetRequest(ctx) {
+
+    }
+
+    static createGame(ctx) {
+
+    }
+
+    static listGames(ctx) {
+
+    }
+
+    static handleNewActions(ctx) {
+
+    }
+
+    static getGameState(ctx) {
+        console.log('getting game state');
+    }
+}
+
+export default Api;
